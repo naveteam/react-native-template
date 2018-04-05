6f6aa62ac4b953364504048f2fd0f86cabc862e6
jpbretanha <jpbretanha@Matheuss-Mac-mini.local>
2018-04-05 16:51:14 -0300

    Redux configurado e adicionado react-navigation

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native';
+import store from './src/store/configureStore';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>Boilerplate padrão!</Text>
+        </View>
+      </Provider>
     );
   }
 }
